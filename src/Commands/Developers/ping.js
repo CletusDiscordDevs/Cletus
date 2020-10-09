1165560fbf8159c9b8504c1ddbc3b8f70bb5bbb0
Ziro <evencoolerbsj@gmail.com>
2020-10-09 08:47:05 -0400

    Added comments

--- src/Commands/Developers/ping.js
+++ src/Commands/Developers/ping.js
@@ -12,6 +12,7 @@ module.exports = {
     category: path.basename(__dirname),
     description: "Checks the bots ping",
     run: async(client, message) => {
+        // You do not nead to add the emoji
     await message.channel.send('<a:googleloading:763951217583849473> Pinging...').then((resultMessage) => {
         const ping = resultMessage.createdTimestamp - message.createdTimestamp
 
