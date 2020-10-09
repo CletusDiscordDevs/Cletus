9152b0a3f34deff718c6d4bc8210cc21b5f371a3
Ziro <evencoolerbsj@gmail.com>
2020-10-08 22:41:45 -0400

    Made ping command will add embed later.

---
+++ src/Commands/Developers/ping.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const path = require('path');
+const util = require('util');
+
+module.exports = {
+    name: "ping",
+    dev: true,
+    usage: {
+        doc: "ping",
+        example: "ping"
+    },
+    category: path.basename(__dirname),
+    description: "Checks the bots ping",
+    run: async(client, message) => {
+    await message.channel.send('<a:googleloading:763951217583849473> Pinging...').then((resultMessage) => {
+        const ping = resultMessage.createdTimestamp - message.createdTimestamp
+
+        message.channel.send(`Bot latency: ${ping}, API Latency: ${client.ws.ping}`)
+
+    })
+    },
+}
