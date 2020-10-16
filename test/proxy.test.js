7007ff5d3f935ed66c7e06bf3a4183c15c2841ea
572512230701006851 <connorwu1010@gmail.com>
2020-10-16 19:45:24 -0400

    Fixed proxy issue

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -3,9 +3,13 @@ const assert = require('assert');
 const fetch = require("node-fetch");
 
 describe('Test Proxy server', async () => {
+    let ProxyResponse = await fetch('https://api.proxyscrape.com/?request=getproxies&proxytype=socks4&timeout=10000&country=all');
+    let proxies = await ProxyResponse.text();
+    let proxy  = proxies.split("\n")[0];
+    let {0: host, 1: port} = proxy.split(":");
     const res = await fetch("https://google.com",{
-        host: "104.238.97.215",
-        port: 36067
+        host,
+        port
     });
     console.log(res)
 });
