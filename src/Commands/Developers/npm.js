bb9ca0b51f472b05897c3389e88cf0d5b1684bee
Ziro <evencoolerbsj@gmail.com>
2020-10-21 12:07:39 -0400

    Fixed some errors along with adding dependancy

--- src/Commands/Developers/npm.js
+++ src/Commands/Developers/npm.js
@@ -13,31 +13,34 @@ module.exports = {
   description: 'Displays information on NPMs',
   run: async (client, message, args) => {
     try {
-  const jsonRes = await fetch(`https://registry.npmjs.com/${args.join(' ')}`).then(res => res.json())
-      if (body.time.unpublished) return message.channel.send('This package no longer exists.');
-      let version = body.versions[body['dist-tags'].latest];
-      let maintainers = body.maintainers.map(user => user.name);
-      let dependencies = version.dependencies ? Object.keys(version.dependencies) : null;
-      let embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor('NPM', 'https://i.imgur.com/ErKf5Y0.png', 'https://www.npmjs.com/')
-        .setTitle(body.name)
-        .setURL(`https://www.npmjs.com/package/${args.join(' ')}`)
-        .setDescription(body.description || 'No description.')
-        .addField('❯ Version', body['dist-tags'].latest, true)
-        .addField('❯ License', body.license || 'None', true)
-        .addField('❯ Author', body.author ? body.author.name : '???', true)
-        .addField('❯ Creation Date', new Date(body.time.created).toDateString(), true)
-        .addField('❯ Modification Date', new Date(body.time.modified).toDateString(), true)
-        .addField('❯ Main File', version.main || 'index.js', true)
-        .addField('❯ Dependencies', dependencies && dependencies.length ? dependencies.join(', ') : 'None')
-        .addField('❯ Maintainers', maintainers.join(', '))
-        .setFooter()
-        .setTimestamp();
-      return message.channel.send(embed);
-    } catch (err) {
-      if (err.statusCode === 200) return message.channel.send('Could not find any results.');
-      return message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
-    };
-  }
-};
+      const jsonRes = await fetch(`https://registry.npmjs.com/${args.join(' ')}`)
+      .then(res => res.json())
+      .then(body => {
+          if (body.time.unpublished) return message.channel.send('This package no longer exists.');
+          let version = body.versions[body['dist-tags'].latest];
+          let maintainers = body.maintainers.map(user => user.name);
+          let dependencies = version.dependencies ? Object.keys(version.dependencies) : null;
+          let embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor('NPM', 'https://i.imgur.com/ErKf5Y0.png', 'https://www.npmjs.com/')
+            .setTitle(body.name)
+            .setURL(`https://www.npmjs.com/package/${args.join(' ')}`)
+            .setDescription(body.description || 'No description.')
+            .addField('❯ Version', body['dist-tags'].latest, true)
+            .addField('❯ License', body.license || 'None', true)
+            .addField('❯ Author', body.author ? body.author.name : 'Unknown Author', true)
+            .addField('❯ Creation Date', new Date(body.time.created).toDateString(), true)
+            .addField('❯ Modification Date', new Date(body.time.modified).toDateString(), true)
+            .addField('❯ Main File', version.main || 'index.js', true)
+            .addField('❯ Dependencies', dependencies && dependencies.length ? dependencies.join(', ') : 'None')
+            .addField('❯ Maintainers', maintainers.join(', '))
+            .setFooter(`Requested by ${message.author.tag}`)
+            .setTimestamp();
+          return message.channel.send(embed);
+      })
+        } catch (err) {
+          if (err.statusCode === 200) return message.channel.send('Could not find any results.');
+          return message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+        };
+      }
+    }
