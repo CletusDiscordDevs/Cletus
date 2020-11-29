3c72d9891f6db3a55d786aa74bd55d5c68b2ed2f
572512230701006851 <connorwu1010@gmail.com>
2020-11-29 16:22:01 -0500

    Added voice state intent

---
+++ tools/utils/clearnpmcache.bat
@@ -0,0 +1,3 @@
+@echo off
+
+powershell -executionpolicy remotedsigned -File .\clearnpmcache.ps1 
---
+++ tools/utils/clearnpmcache.ps1
@@ -0,0 +1,5 @@
+Remove-Item -Recurse $env:APPDATA\npm-cache
+
+Remove-Item -Recurse $env:USERPROFILE\AppData\Local\npm-cache
+
+Remove-Item -Recurse $env:USERPROFILE\AppData\Local\node-gyp
